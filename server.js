f216e0c2b048a6d6667ec6e1e91d0dd46f9636b5
Sahil <smehar@aceproject.space>
2021-05-22 13:48:19 -0500

    Backend setup

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+
+//Assigning the port to the backend server
+const port = process.env.PORT || 5000;
+
+//Creating a server
+const app = express();
+
+app.use(cors());
+//It will let the application know that we are using json format
+app.use(express.json());
+
+app.get('/', (req,res) =>{
+    res.send('Hello World');
+})
+
+app.listen(port,()=>{
+    console.log(`The Server is running on PORT NO: ${port}`);
+})
